4bd64bfb1a898ed7b4d489e07ed9968061558163
Vitaly S <32413174+vit-s@users.noreply.github.com>
2023-09-10 22:35:05 +0300

    car-comm

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,52 @@
-import { Hero } from '@/components'
-import Image from 'next/image'
+import { Hero, CustomFilter, SearchBar, CarCard } from "../components/index";
+import Image                                      from 'next/image'
+import { fetchCars }                from "../utils/index";
+import { fuels, yearsOfProduction } from "../constants/index";
 
-export default function Home() {
+export default async function Home({ searchParams }) {
+
+  const allCars = await fetchCars({
+    manufacturer: searchParams.manufacturer || '',
+    year: searchParams.year || 2022,
+    fuel: searchParams.fuel || '',
+    limit: searchParams.limit || 10,
+    model: searchParams.model || '',
+  });
+
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
   return (
     <main className="overflow-hidden">
-      <Hero />
+      <Hero/>
+      <div className="mt-12 padding-x padding-y max-width" id={'discover'}>
+        <div className="home_text-container">
+          <h1 className="text-4x1 font-extrabold">
+            Car Catalogue
+          </h1>
+          <p>Explore the cars you might like</p>
+        </div>
+        <div className="home__filters">
+          <SearchBar/>
+          <div className="home__filter-container">
+            <CustomFilter title={`fuel`} options={fuels}/>
+            <CustomFilter title={`year`} options={yearsOfProduction}/>
+          </div>
+        </div>
+        {
+          !isDataEmpty ? (
+            <section>
+              <div className={`home__cars-wrapper`}>
+                {allCars?.map(car => (<CarCard car={car}/>))}
+              </div>
+            </section>
+          ) : (
+            <div className={`home__error-container`}>
+              <h2 className={`text-black text-x1 font-bold`}>Oops, no results</h2>
+              <p>{allCars?.message}</p>
+            </div>
+          )
+        }
+      </div>
     </main>
-  )
-}
+  );
+};
---
+++ components/SearchBar.tsx
@@ -0,0 +1,70 @@
+"use client"
+import { SearchManufacturer } from "./index";
+import React, { useState }    from "react";
+import Image                  from "next/image";
+import { useRouter }          from "next/navigation";
+
+const SearchButton = ({otherClasses}: { otherClasses: string }) => (
+  <button type={`submit`} className={`-ml-3 z-10 ${otherClasses}`}>
+    <Image
+      src={'./magnifying-glass.svg'}
+      alt={'magnifying glass'}
+      width={40}
+      height={40}
+      className={`object-contain`}
+    />
+  </button>
+)
+
+const SearchBar = () => {
+  const [manufacturer, setManufacturer] = useState("");
+  const [model, setModel]               = useState("");
+  const handleSearch                    = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (manufacturer === '' && model === '') {
+      return alert('Please fill in the search bar')
+    }
+    updateSearchParams(model.toLowerCase(), manufacturer.toLowerCase())
+  }
+  const router                          = useRouter();
+  const updateSearchParams              = (model: string, manufacturer: string) => {
+    const searchParams = new URLSearchParams(window.location.search)
+    model ? searchParams.set('model', model) : searchParams.delete('model')
+    manufacturer ? searchParams.set('manufacturer', manufacturer) : searchParams.delete('manufacturer')
+    const newPathName = `${window.location.pathname}?${searchParams.toString()}`
+    router.push(newPathName)
+  }
+
+  return (
+    <form className='searchbar' onSubmit={handleSearch}>
+      <div className='searchbar__item'>
+        <SearchManufacturer
+          manufacturer={manufacturer}
+          setManufacturer={setManufacturer}
+        />
+        <SearchButton otherClasses={`sm:hidden`}/>
+      </div>
+      <div className={`searchbar__item`}>
+        <Image
+          src={'/model-icon.png'}
+          alt={'car model'}
+          width={25}
+          height={25}
+          className={`absolute w-[20px] h-[20px] ml-4`}
+        />
+        <input
+          type="text"
+          name={`model`}
+          value={model}
+          onChange={(e) => setModel(e.target.value)}
+          placeholder={`Tiguan`}
+          className={`searchbar__input`}
+        />
+        <SearchButton otherClasses={`sm:hidden`}/>
+      </div>
+      <SearchButton otherClasses={`max-sm:hidden`}/>
+    </form>
+  );
+};
+
+export default SearchBar;
